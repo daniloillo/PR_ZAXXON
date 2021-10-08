8da9677eab2dc82fba8f23478bdd2617aae0644e
Sergio Fernández <daniellopezblanco26@gmail.com>
2021-10-08 17:27:59 +0200

    Commit 2 Movimiento Nave
    Commit con el script de movimiento de nave y el script de Variables publicas

---
+++ PR_ZAXXON_LopezDaniel/Assets/Scripts/NaveMove.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveMove : MonoBehaviour
+{
+    //VELOCIDAD
+    float desplXSpeed;
+    float desplYSpeed;
+
+    Variables variables;
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        variables = GameObject.Find("Variables").GetComponent<Variables>();
+
+        desplXSpeed = variables.ShipSpeedX;
+        desplYSpeed = variables.ShipSpeedY;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        Movimiento();
+
+
+
+    }
+
+    void Movimiento()
+    {
+        //INTERACTIVIDAD
+        float desplX = Input.GetAxis("Horizontal");
+
+        float desplY = Input.GetAxis("Vertical");
+
+        float rollR = Input.GetAxis("RollR");
+
+        float rollL = Input.GetAxis("RollL");
+
+        
+
+        //POSICION
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+
+        //LIMITE HORIZONTAL
+        float limX = 20f;
+        
+
+        //LIMITES VERTICALES
+        float limYU = 15f;
+        float limYD = 0f;
+
+
+
+        transform.Translate(Vector3.right * Time.deltaTime * desplX * desplXSpeed, Space.World);
+
+        transform.Translate(Vector3.up * Time.deltaTime * desplY * desplYSpeed, Space.World);
+
+        transform.Rotate(Vector3.forward * Time.deltaTime * rollR * 100f);
+
+        transform.Rotate(Vector3.back * Time.deltaTime * rollL * 100f);
+
+        
+
+        //LIMITES
+
+        if (posX > limX && desplX > 0f)
+        {
+
+            transform.position = new Vector3(limX, transform.position.y, transform.position.z);
+
+        }
+        if (posX < -limX && desplX < 0f)
+        {
+
+            transform.position = new Vector3(-limX, transform.position.y, transform.position.z);
+
+        }
+        if (posY > limYU && desplY > 0f)
+        {
+
+            transform.position = new Vector3(transform.position.x, limYU, transform.position.z);
+
+        }
+        if (posY < limYD && desplY < 0f)
+        {
+            transform.position = new Vector3(transform.position.x, limYD, transform.position.z);
+        }
+
+
+        //NO FUNCIONA
+
+        /*if((posX < limX || desplX < 0f) && (posX > -limX || desplX > 0f))
+        {
+
+            transform.Translate(Vector3.right * Time.deltaTime * desplX * desplXSpeed, Space.World);
+
+        }
+
+        if((posY < limYU || desplY < 0f) && (posY > limYD || desplY > 0f))
+        {
+
+            transform.Translate(Vector3.up * Time.deltaTime * desplY * desplYSpeed, Space.World);
+
+        }
+        */
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
